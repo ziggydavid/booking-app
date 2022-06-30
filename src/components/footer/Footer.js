7769c6ae6ffe80ce4dbdc749c951e756ebe91f74
ziggydavid <amuhdavid@gmail.com>
2022-06-30 04:48:58 +0100

    maillist and footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='fLists'>
+            <ul className='fList'>
+                <li className='fListItem'>Countries</li>
+                <li className='fListItem'>Regions</li>
+                <li className='fListItem'>Cities</li>
+                <li className='fListItem'>Districts</li>
+                <li className='fListItem'>Airports</li>
+                <li className='fListItem'>Hotels</li>
+                <li className='fListItem'>Places of Interest</li>
+            </ul>
+            <ul className='fList'>
+                <li className='fListItem'>Countries</li>
+                <li className='fListItem'>Regions</li>
+                <li className='fListItem'>Cities</li>
+                <li className='fListItem'>Districts</li>
+                <li className='fListItem'>Airports</li>
+                <li className='fListItem'>Hotels</li>
+                <li className='fListItem'>Places of Interest</li>
+            </ul>
+            <ul className='fList'>
+                <li className='fListItem'>Countries</li>
+                <li className='fListItem'>Regions</li>
+                <li className='fListItem'>Cities</li>
+                <li className='fListItem'>Districts</li>
+                <li className='fListItem'>Airports</li>
+                <li className='fListItem'>Hotels</li>
+                <li className='fListItem'>Places of Interest</li>
+            </ul>
+            <ul className='fList'>
+                <li className='fListItem'>Countries</li>
+                <li className='fListItem'>Regions</li>
+                <li className='fListItem'>Cities</li>
+                <li className='fListItem'>Districts</li>
+                <li className='fListItem'>Airports</li>
+                <li className='fListItem'>Hotels</li>
+                <li className='fListItem'>Places of Interest</li>
+            </ul>
+            <ul className='fList'>
+                <li className='fListItem'>Countries</li>
+                <li className='fListItem'>Regions</li>
+                <li className='fListItem'>Cities</li>
+                <li className='fListItem'>Districts</li>
+                <li className='fListItem'>Airports</li>
+                <li className='fListItem'>Hotels</li>
+                <li className='fListItem'>Places of Interest</li>
+            </ul>
+        </div>
+        <div className='footerText'>Copyright © 2022 ziggybooking</div>
+        
+    </div>
+  )
+}
+
+export default Footer
